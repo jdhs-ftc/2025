1d8e3c55d8672eb20a22fea0eb7b289f176fc67e
j5155 <54331556+j5155@users.noreply.github.com>
2025-09-07 18:22:18 -0800

    repacakge rr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/messages/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/messages/TankDrive.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.rr.messages;
 
 import androidx.annotation.NonNull;
 
@@ -43,15 +43,9 @@ import com.acmerobotics.roadrunner.ftc.RawEncoder;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
-
-import org.firstinspires.ftc.teamcode.messages.DriveCommandMessage;
-import org.firstinspires.ftc.teamcode.messages.PoseMessage;
-import org.firstinspires.ftc.teamcode.messages.TankCommandMessage;
-import org.firstinspires.ftc.teamcode.messages.TankLocalizerInputsMessage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
